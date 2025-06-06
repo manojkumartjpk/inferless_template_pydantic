00ca3aa98b6c4adcbcff3773b1f90404983e82e1
manojkumartjpk <45778460+manojkumartjpk@users.noreply.github.com>
2025-06-06 15:14:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,5 +51,5 @@ class InferlessPythonModel:
         return generateObject
 
     # perform any cleanup activity here
-    def finalize(self,args):
+    def finalize(self):
         self.pipe = None
